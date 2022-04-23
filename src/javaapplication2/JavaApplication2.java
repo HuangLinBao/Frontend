057899d1c6c7098dfc8ef3a28d43a130feb85f7e
Tariq Sabri <tariq.92@live.com>
2022-04-23 14:38:04 +0300

    eyyy

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author mahmood
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       LogIn log=new LogIn();
+       log.setVisible(true);
+        employee frame = new employee();
+        frame.setSize(600,420);
+        frame.setVisible(true);
+    }
+    
+}
